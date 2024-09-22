1dd908f6c17a07a42a6fb0e7a73db2eef0eeed6b
Chayan <chayankela6@gmail.com>
2024-09-23 00:32:05 +0530

    cors fix

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const { connectToMongoDB } = require("./database");
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  const allowedOrigin = process.env.ALLOWED_ORIGIN;
-  if (req.headers.origin === allowedOrigin) {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   const allowedOrigin = process.env.ALLOWED_ORIGIN;
+//   if (req.headers.origin === allowedOrigin) {
+//     res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
+
+const cors = require("cors");
+app.use(cors());
 
 const router = require("./routes");
 app.use("/api", router);
