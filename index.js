11a88ecfc4936737491da9027e52976837595fdd
Chayan <chayankela6@gmail.com>
2024-09-22 19:08:04 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -10,15 +9,11 @@ var cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
-});
 
 const router = require("./routes");
 app.use("/api", router);
 
-const port = 5000;
+const port = process.env.PORT;
 
 async function startServer() {
   await connectToMongoDB();
